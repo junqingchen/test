360a05dbac1fad1ee6f32756a068f88c1828a5cd
chenjunqing <377615720@qq.com>
2017-06-01 20:28:56 +0800

    添加service层

---
+++ myfirst/src/com/cyyz/myfirst/dao/com/cyyz/myfirst/service/BaseService.java
@@ -0,0 +1,8 @@
+package com.cyyz.myfirst.dao.com.cyyz.myfirst.service;
+
+/**
+ * Created by jq.chen on 2017/6/1.
+ */
+public interface BaseService {
+    void add(int id,String name,String password);
+}
---
+++ myfirst/src/com/cyyz/myfirst/dao/com/cyyz/myfirst/service/BaseServiceImpl.java
@@ -0,0 +1,11 @@
+package com.cyyz.myfirst.dao.com.cyyz.myfirst.service;
+
+/**
+ * Created by jq.chen on 2017/6/1.
+ */
+public class BaseServiceImpl implements BaseService{
+    @Override
+    public void add(int id, String name, String password) {
+
+    }
+}
