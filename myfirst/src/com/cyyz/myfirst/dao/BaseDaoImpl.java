2aa46baa27bfe747248199e7e00bf5c62ea0847f
chenjunqing <377615720@qq.com>
2017-06-01 19:48:37 +0800

    添加接口实现类

---
+++ myfirst/src/com/cyyz/myfirst/dao/BaseDaoImpl.java
@@ -0,0 +1,38 @@
+package com.cyyz.myfirst.dao;
+
+/**
+ * Created by jq.chen on 2017/6/1.
+ */
+public class BaseDaoImpl extends  BaseDao{
+
+
+    @Override
+    public void save(int id, String name, String password) {
+
+    }
+
+    @Override
+    public void update(int id, String name, String password) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public String getName(int id) {
+        return null;
+    }
+
+    @Override
+    public void test(String name) {
+
+    }
+
+    @Override
+    public Stirng getPassword(String id) {
+        return null;
+    }
+}
