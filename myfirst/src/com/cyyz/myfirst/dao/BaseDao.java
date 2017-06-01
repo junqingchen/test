3f7f1a28a445b924b81c5d7d9da5f4b33257e06f
chenjunqing <377615720@qq.com>
2017-06-01 19:58:03 +0800

    添加注释

--- myfirst/src/com/cyyz/myfirst/dao/BaseDao.java
+++ myfirst/src/com/cyyz/myfirst/dao/BaseDao.java
@@ -4,6 +4,12 @@ package com.cyyz.myfirst.dao;
  * Created by jq.chen on 2017/6/1.
  */
 interface BaseDao {
+    /**
+     * 添加一条新的记录
+     * @param id
+     * @param name
+     * @param password
+     */
     void save(int id,String name,String password);
     void update(int id,String name,String password);
     void delete(int id);
