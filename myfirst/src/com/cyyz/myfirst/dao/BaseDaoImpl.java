b8185a31193046b74a76c94f2f9a7a09d99c251e
chenjunqing <377615720@qq.com>
2017-06-01 20:17:53 +0800

    添加注释

---
+++ myfirst/src/com/cyyz/myfirst/dao/BaseDaoImpl.java
@@ -0,0 +1,38 @@
+package com.cyyz.myfirst.dao;
+
+/**
+ * Created by jq.chen on 2017/6/1.
+ */
+public class BaseDaoImpl implements  BaseDao{
+    /**
+     * 添加一条新的记录
+     *
+     * @param id       用户ID
+     * @param name     用户名称
+     * @param password 用户密码
+     */
+    @Override
+    public void save(int id, String name, String password) {
+
+    }
+
+    @Override
+    public void update(int id, String name, String password) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public String getName(int id) {
+        return null;
+    }
+
+    @Override
+    public void test(String name) {
+
+    }
+}
